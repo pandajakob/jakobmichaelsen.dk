21c9d25c357d9b4cb3bcf5075fdb0e49db0e2653
Jakob Michaelsen <jakobm.dev@gmail.com>
2025-01-17 16:34:56 +0100

    Index.html changes

--- index.js
+++ index.js
@@ -2,13 +2,17 @@ const express = require('express');
 const app = express();
 const fs = require('fs');
 const path = require('path');
+
 // Define a port
-const PORT = 3000;
+const PORT = 5000;
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Root route to serve HTML
 app.get('/', (req, res) => {
-     res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
+
 
 // Start the server
 app.listen(PORT, () => {
