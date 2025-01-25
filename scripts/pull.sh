455789eef36ce70e587b2c6d4e36e258756b1e4e
pandajakob <jakobm.dev@gmail.com>
2025-01-25 12:17:42 +0100

    date in pull.sh

--- scripts/pull.sh
+++ scripts/pull.sh
@@ -2,7 +2,8 @@ cd /home/jakob/Desktop/jakobmichaelsen.dk/ || {
   echo "Error: Directory not found. Exiting.";
   exit 1;
 }
+now="$(date +'%Y-%m-%d %H:%M:%S')"
 
 # Pull the latest changes
-echo "Pulling latest changes in $(pwd)..."
+echo "$now Pulling latest changes in $(pwd)..."
 sudo git pull
