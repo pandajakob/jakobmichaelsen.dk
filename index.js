95970fa1a9deeec07706278046e22508dc8f016d
pandajakob <jakobm.dev@gmail.com>
2025-02-15 14:46:24 +0100

    index.js

--- index.js
+++ index.js
@@ -1,14 +1,40 @@
 const express = require('express');
 const { createServer } = require('node:http');
+const { exec } = require("child_process");
+const bodyParser = require("body-parser");
 
 const fs = require('fs');
 const path = require('path');
 const PORT = 5000;
 
 const app = express();
-const server = createServer(app);
+
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+app.use(bodyParser.json());
+
+app.post("/webhook", (req, res) => {
+    const payload = req.body;
+	console.log("Webhook received")
+    if (payload.ref === "refs/heads/main") {
+        console.log("Changes detected on main. Pulling latest changes...");
+
+		exec("cd /home/jakob/Desktop/jakobmichaelsen.dk/ && sudo git fetch --all && sudo git reset --hard origin/main && sudo git pull", (err, stdout, stderr) => {
+            if (err) {
+                console.error(`Error: ${stderr}`);
+                return res.status(500).send("Git pull failed");
+            }
+            console.log(`Git Pull Output: ${stdout}`);
+            res.status(200).send("Updated successfully");
+        });
+    } else {
+        res.status(200).send("Not on main, ignoring...");
+    }
+});
+
 
 
 app.get('/projects', (req, res) => {
